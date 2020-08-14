a3b24975ee0619878eccc40e2e780d794de03603
shun shimono <shunshun5589@gmail.com>
2020-08-15 00:34:35 +0900

    型追加

---
+++ src/array.ts
@@ -0,0 +1,11 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+console.log({ numbers });
+
+let nummbers2: Array<number> = [1, 2, 3];
+
+let strings: Array<string> = ["type", "java"];
+
+let sammple: (string | number | boolean)[] = [1, false, "japan", "shimono"];
