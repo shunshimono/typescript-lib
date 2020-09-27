869375a9bc3366324e0c4a7bfb5acc36a8dd5dbf
shun shimono <shunshun5589@gmail.com>
2020-09-27 22:19:27 +0900

    高度な型

--- src/docAdvanceType.ts
+++ src/docAdvanceType.ts
@@ -1,3 +1,4 @@
+export {};
 /**
  * Intersection Types(交差型)
  * 複数の型を一つの型に結合することが可能 &で表現
@@ -36,5 +37,56 @@ numberOrString = [1, "1"];
 
 /**
  * Literal Types(文字列リテラル)
- * 複数の型をうち一つが成立するばOK |で表現
+ * 正確な値の型をしてるすることができる
  */
+let myName: "shun";
+
+/**
+ * typeof
+ * 宣言済み変数の型を取得することが可能
+ * 型クエリーを共呼ばれる
+ * 型推論と組み合わせた場合のみ有効
+ */
+let asString: string;
+let asValue: typeof asString;
+
+let myObject = { foo: "foo" };
+let anotherObject: typeof myObject = { foo: "" };
+
+/**
+ * keyof
+ * オブジェクトのプロパティ名称を String Literal Types　で取得可能
+ */
+type someType = {
+  foo: string;
+  bar: string;
+  baz: string;
+};
+
+let someKey: keyof someType;
+
+const myObj = {
+  foo: "foo",
+  bar: "bar",
+  baz: "baz",
+};
+
+let myObjKey: keyof typeof myObj;
+
+/**
+ * アサーション
+ * 型の抽象度が低い型に対して、高くすることができる
+ */
+let someValue: any = "this any value";
+let strLength: number = (someValue as string).length;
+
+/**
+ * enum
+ * 列挙型
+ */
+enum Direction {
+  Up, // 0
+  Down, // 1
+  Left, // 2
+  Right, // 3
+}
