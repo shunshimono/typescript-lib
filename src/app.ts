51b37bd71efb036528a9759afab5c1b6a15f203a
shun shimono <shunshun5589@gmail.com>
2020-10-29 15:05:46 +0900

    push

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,28 @@
-interface App {
-  name: string;
-  age: number;
-}
+// interface App {
+//   name: string;
+//   age: number;
+// }
 
-interface myApp<P> extends App {
-  baz: number;
-  foo: P;
-}
-const sample: myApp<number> = {
-  name: "shun",
-  age: 24,
-  baz: 0,
-  foo: 1,
+// interface myApp<P> extends App {
+//   baz: number;
+//   foo: P;
+// }
+// const sample: myApp<number> = {
+//   name: "shun",
+//   age: 24,
+//   baz: 0,
+//   foo: 1,
+// };
+
+export {};
+
+type A = {
+  hoge: string;
+  piyo: number;
 };
+
+let o1: A;
+
+const o2 = { hoge: "", piyo: 123, foo: "foo" };
+
+o1 = o2; // OK
---
+++ src/function.ts
@@ -0,0 +1,16 @@
+export {};
+
+// function* create() {
+//   let a = 1;
+//   let b = 2;
+//   while (true) {
+//     yield a;
+//     [a, b] = [b, a + b];
+//   }
+// }
+
+// const sample = create();
+// console.log(sample.next());
+// console.log(sample.next());
+// console.log(sample.next());
+// console.log(sample.next());
